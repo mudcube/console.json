b143a2beb0ed42d094d8fcd02aa33bba5cde518c
Michael Deal <mudcube@gmail.com>
2015-03-29 02:53:48 -0700

    cleanup, make easily to template

--- script.js
+++ script.js
@@ -1,47 +1,51 @@
 console.json = function(json) {
-	var background = '; background: #333; padding: 2px 0;';
+	var template = {
+		'background': '#333',
+		'bracket': '#777',
+		'string': '#B0DC19',
+		'boolean': '#80DEF9',
+		'number': '#FA9D11',
+		'null': '#D93B6C',
+		'undefined': '#D93B6C'
+	};
+	///
+	var background = '; background: ' + template.background + '; padding: 2px 0;';
 	var paddingRight = 'padding-right: 10px;';
-	var defaultColor = '#777';
+	var defaultColor = template.bracket;
 	///
 	var printBracket = function(indent, value, suffix) {
-		console.log(
-			'%c ' + indent + value + (suffix || ''), 
-			'color: ' + defaultColor + background + paddingRight
-		);
+		console.log('%c ' + indent + value + (suffix || ''), 'color: ' + defaultColor + background + paddingRight);
 	};
-	
-    var printVariable = function(indent, key, value, suffix) {
+
+	var printVariable = function(indent, key, value, suffix) {
 		var color = defaultColor;
-		switch(typeof value) {
-			case 'string':
-				color = '#B0DC19'; // string
-				value = '"' + value + '"';
-				break;
-			case 'boolean':
-				color = '#80DEF9'; // boolean
-				break;
-			case 'number':
-				color = '#FA9D11'; // number
-				break;
-			case 'object': // null
-			case 'undefined':
-				color = '#D93B6C';
-				break;
+		switch (typeof value) {
+		case 'string':
+			color = template.string;
+			value = '"' + value + '"';
+			break;
+		case 'boolean':
+			color = template.boolean;
+			break;
+		case 'number':
+			color = template.number;
+			break;
+		case 'object':
+			color = template.null;
+			break;
+		case 'undefined':
+			color = template.undefined;
+			break;
 		}
-		console.log(
-			'%c' + indent + key + '%c ' + value + '%c' + (suffix || ''), 
-			'color: ' + defaultColor + background, 
-			'color: ' + color + background, 
-			'color: ' + defaultColor + background + paddingRight
-		);
+		console.log('%c' + indent + key + '%c ' + value + '%c' + (suffix || ''), 'color: ' + defaultColor + background, 'color: ' + color + background, 'color: ' + defaultColor + background + paddingRight);
 	};
 	///
-    var recurse = function(data, indent) {
+	var recurse = function(data, indent) {
 		var prev;
 		var vindent = indent + '\t';
 		///
 		if (Array.isArray(data)) {
-			for (var n = 0, len = data.length; n < len; n ++) {
+			for (var n = 0, len = data.length; n < len; n++) {
 				var key = n + ':';
 				var value = data[n];
 				var suffix = n === len - 1 ? '' : ',';
@@ -64,7 +68,7 @@ console.json = function(json) {
 						recurse(value, vindent);
 					}
 					prev = undefined;
-				} else { 
+				} else {
 					prev = [vindent, '"' + key + '":', value];
 				}
 			}
@@ -75,6 +79,6 @@ console.json = function(json) {
 		}
 	};
 	///
-    printBracket('', '{');
-    recurse(json, '');
+	printBracket('', '{');
+	recurse(json, '');
 };
