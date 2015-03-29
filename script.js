86a79e09aec717b19477083c555d9124b928afe9
Michael Deal <mudcube@gmail.com>
2015-03-29 02:57:09 -0700

    cleanup

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 console.json = function(json) {
 	var template = {
 		'background': '#333',
-		'bracket': '#777',
+		'default': '#777',
 		'string': '#B0DC19',
 		'boolean': '#80DEF9',
 		'number': '#FA9D11',
@@ -11,33 +11,36 @@ console.json = function(json) {
 	///
 	var background = '; background: ' + template.background + '; padding: 2px 0;';
 	var paddingRight = 'padding-right: 10px;';
-	var defaultColor = template.bracket;
+	var defaultColor = template.default;
 	///
 	var printBracket = function(indent, value, suffix) {
-		console.log('%c ' + indent + value + (suffix || ''), 'color: ' + defaultColor + background + paddingRight);
+		console.log(
+			'%c ' + indent + value + (suffix || ''), 
+			'color: ' + defaultColor + background + paddingRight
+		);
 	};
 
 	var printVariable = function(indent, key, value, suffix) {
 		var color = defaultColor;
-		switch (typeof value) {
-		case 'string':
-			color = template.string;
-			value = '"' + value + '"';
-			break;
-		case 'boolean':
-			color = template.boolean;
-			break;
-		case 'number':
-			color = template.number;
-			break;
-		case 'object':
-			color = template.null;
-			break;
-		case 'undefined':
-			color = template.undefined;
-			break;
+		var type = typeof value;
+		switch (type) {
+			case 'string':
+				color = template.string;
+				value = '"' + value + '"';
+				break;
+			case 'object':
+				color = template.null;
+				break;
+			default:
+				color = template[type];
+				break;
 		}
-		console.log('%c' + indent + key + '%c ' + value + '%c' + (suffix || ''), 'color: ' + defaultColor + background, 'color: ' + color + background, 'color: ' + defaultColor + background + paddingRight);
+		console.log(
+			'%c' + indent + key + '%c ' + value + '%c' + (suffix || ''), 
+			'color: ' + defaultColor + background, 
+			'color: ' + color + background, 
+			'color: ' + defaultColor + background + paddingRight
+		);
 	};
 	///
 	var recurse = function(data, indent) {
@@ -80,5 +83,6 @@ console.json = function(json) {
 	};
 	///
 	printBracket('', '{');
+	///
 	recurse(json, '');
 };
