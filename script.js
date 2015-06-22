349ef6ad4838daf2849a8454296f529885411162
Michael Deal <mudcube@gmail.com>
2015-06-21 22:08:35 -0700

    update color scheme, add custom 'delimiter'

--- script.js
+++ script.js
@@ -1,19 +1,32 @@
-console.json = function(json) {
+/*
+	console.json({
+	    'string': 'testing',
+	    'boolean': true,
+	    'function': function() {},
+	    'number': 3123,
+	    'null': null,
+	    'undefined': undefined
+	});
+*/
+
+console.json = function(object, depth, delimiter) {
+	depth = depth || 10; //- implement me to prevent recursion
+	delimiter = delimiter || '\t';
 
 	var template = {
-		'background': 'background: #333;',
-		'default': 'color: #777;',
-		'string': 'color: #B0DC19;',
-		'boolean': 'color: #80DEF9;',
-		'number': 'color: #FA9D11;',
-		'null': 'color: #D93B6C;',
-		'undefined': 'color: #D93B6C;'
+		'background': 'background: #1B2B34;', // dark-blue
+		'default': 'color: #CDD3DE;', // gray
+		'string': 'color: #99C794;', // green
+		'boolean': 'color: #6699CC;', // blue
+		'function': 'color: #EC5f67;', // red
+		'number': 'color: #FAC863;', // yellow
+		'null': 'color: #F99157;', // orange
+		'undefined': 'color: #C594C5;' // purple
 	};
-	///
-	var background = template.background + '; padding: 2px 0;';
-	var paddingRight = 'padding-right: 10px;';
-	///
-	var log = function(indent, type, arg1, arg2, arg3) {
+
+	function log(indent, type, arg1, arg2, arg3) {
+		var background = template.background + '; padding: 3px 0;';
+		var paddingRight = 'padding-right: 10px;';
 		var stylePrefix = template.default + background;
 		var styleSuffix = template.default + background + paddingRight;
 		///
@@ -22,24 +35,29 @@ console.json = function(json) {
 				console.log('%c' + indent + arg1, styleSuffix);
 				return;
 			case 'string':
-				color = template.string;
+				color = template['string'];
 				arg2 = '"' + arg2 + '"';
 				break;
 			case 'object':
-				color = template.null;
+				color = template['null'];
+				break;
+			case 'function':
+				color = template['function'];
+				arg2 = 'function(){},';
 				break;
 			default:
 				color = template[type];
 				break;
 		}
+		///
 		console.log(
 			'%c' + indent + arg1 + '%c' + arg2 + '%c' + (arg3 || ''), 
 			stylePrefix, color + background, styleSuffix
 		);
 	};
-	///
-	var processObject = function(data, vindent) {
-		var printBuffer = function(addComma) {
+
+	function processObject(data, vindent) {
+		function printBuffer(addComma) {
 			if (buffer) {
 				if (addComma) {
 					if (buffer[1] === 'default') {
@@ -52,7 +70,7 @@ console.json = function(json) {
 			}
 		};
 		
-		var processArg = function(key, value) {
+		function processArg(key, value) {
 			printBuffer(true);
 			if (value && typeof value === 'object') {
 				var suffix = Array.isArray(value) ? '[' : '{';
@@ -64,12 +82,12 @@ console.json = function(json) {
 		};
 		///
 		var buffer;
-		var indent = vindent + '\t';
+		var indent = vindent + delimiter;
 		///
 		if (Array.isArray(data)) {
 			var suffix = ']';
 			for (var idx = 0, length = data.length; idx < length; idx++) {
-				processArg(idx + ':', data[idx]);
+				processArg(idx + ': ', data[idx]);
 			}
 		} else {
 			var suffix = '}';
@@ -83,6 +101,6 @@ console.json = function(json) {
 	};
 	///
 	log('', 'default', '{');
-	log.apply(null, processObject(json, ''));
+	log.apply(null, processObject(object, ''));
 
 };
